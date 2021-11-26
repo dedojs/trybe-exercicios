5c3be979b97e54e0a905cd6be17b4f44ca179711
dedojs <xandresousax@gmail.com>
2021-11-26 18:23:39 -0300

    Exercicios concluidos

--- fundamentos/bloco-04-introducao-javascript/exercicios.js
+++ fundamentos/bloco-04-introducao-javascript/exercicios.js
@@ -114,3 +114,185 @@ switch(xadrez){
     default:
         console.log("Peça inválida")
 }
+
+/* 7 Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+Porcentagem >= 90 -> A
+Porcentagem >= 80 -> B
+Porcentagem >= 70 -> C
+Porcentagem >= 60 -> D
+Porcentagem >= 50 -> E
+Porcentagem < 50 -> F
+O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100. */
+
+var nota = 85.5
+
+if(nota>=90){
+    console.log("A")
+}
+else if(nota >= 80){
+    console.log("B")
+}
+else if(nota>=70){
+    console.log("C")
+}
+else if(nota>=60){
+    console.log("D")
+}
+else if(nota>=50){
+    console.log("E")
+}
+else if(nota<50){
+    console.log("F")
+}
+
+/* 8 Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
+Bonus: use somente um if . */
+
+const r = 81
+const k = 241
+const p = 9372
+
+if(r%2 == 0 || k%2 == 0 || p%2 == 0){
+    console.log(true, "par")
+}
+else{
+    console.log(false)
+}
+
+/*9 Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+Bonus: use somente um if .*/
+
+const r1 = 81
+const k1 = 241
+const p1 = 9372
+
+if(r1%2 == 1 || k1%2 == 1 || p1%2 == 1){
+    console.log(true, "impar")
+}
+else{
+    console.log(false)
+}
+
+/* 10 Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+Atente que, sobre o custo do produto, incide um imposto de 20%.
+Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
+O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
+valorCustoTotal = valorCusto + impostoSobreOCusto
+lucro = valorVenda - valorCustoTotal (lucro de um produto) */
+
+const custo = 100
+const venda = 150
+var vendidos = 100
+var imposto = custo * 0.20 + custo
+var lucro = (venda * vendidos)-(imposto*vendidos)
+
+if(custo == 0 || venda ==0){
+    console.log("ERRO na informação dos valores")
+}
+else{
+    console.log("O lucro foi de ", lucro)
+}
+
+/*Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
+
+A notação para um salário de R$1500,10, por exemplo, deve ser 1500.10. Para as faixas de impostos, use as seguintes referências:
+
+INSS (Instituto Nacional do Seguro Social)
+Salário bruto até R$ 1.556,94: alíquota de 8%
+Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%
+Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11%
+Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88
+IR (Imposto de Renda)
+
+Até R$ 1.903,98: isento de imposto de renda
+De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
+De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
+De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
+Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
+
+Exemplo : Uma pessoa possui o salário bruto de R$ 3.000,00. O cálculo será:
+
+O salário bruto está entre R$ 2.594,93 e R$ 5.189,82, então sua alíquota para INSS é de 11%. O INSS será 11% de R$ 3.000, ou seja, R$ 330,00.
+Para descobrir o salário-base, subtraia do salário bruto a alíquota do INSS: R$ 3.000,00 - R$ 330,00 = R$ 2.670,00.
+
+Para pegar o valor do IR, temos um salário (já deduzido o INSS) entre R$ 1.903,99 e 2.826,65, sendo a alíquota, então, de 7.5%, com parcela de R$ 142,80 a deduzir do imposto. 
+
+Assim, temos:
+R$ 2.670,00: salário com INSS já deduzido;
+7.5%: alíquota de imposto de renda;
+R$ 142,80 parcela a se deduzir do imposto.
+
+Fazendo a conta, temos: (7,5% de R$ 2.670,00) - R$ 142,80 = R$ 57,45
+O último cálculo para conseguir o salário líquido é R$ 2.670,00 - R$ 57,45 (salário-base - valor IR) = R$ 2.612,55.
+Resultado: R$ 2.612,55.
+
+Dica: que tal identificar as alíquotas com variáveis de nomes explicativos?*/
+
+/*INSS (Instituto Nacional do Seguro Social)
+Salário bruto até R$ 1.556,94: alíquota de 8%
+Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%
+Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11%
+Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88*/
+
+
+var inss = 0;
+var ir = 0;
+var salarioBruto = 3000;
+var salarioBase = 0;
+var salarioLiquido = salarioBase - ir;
+
+if(salarioBruto > 5189.82){
+    inss = 570.88
+    salarioBase = salarioBruto - inss;
+}
+else if(salarioBruto > 2594.93){
+    inss = salarioBruto*0.11
+    salarioBase = salarioBruto - inss;
+}
+else if(salarioBruto > 1556.95){
+    inss = salarioBruto*0.09
+    salarioBase = salarioBruto - inss;
+}
+else if(salarioBruto <= 1556.94){
+    inss = salarioBruto * 0.08
+    salarioBase = salarioBruto - inss;
+}
+
+/*Até R$ 1.903,98: isento de imposto de renda
+De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
+De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
+De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
+Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.*/
+
+
+if(salarioBase> 4664.68){
+    ir = salarioBase * 0.275 - 869.36
+    salarioLiquido = salarioBase - ir;
+}
+else if(salarioBase > 3751.06){
+    ir = salarioBase * 0.225 - 636.13
+    salarioLiquido = salarioBase - ir;
+}
+else if(salarioBase > 2826.66){
+    ir = salarioBase * 0.15 - 354.80
+    salarioLiquido = salarioBase - ir;
+}
+else if(salarioBase > 1903,99){
+    ir = (salarioBase * 0.075) - 142.80
+    salarioLiquido = salarioBase - ir;
+}
+else if(salarioBase <= 1903,98){
+    ir = 0
+    salarioLiquido = salarioBase - ir;
+}
+
+/*O último cálculo para conseguir o salário líquido é R$ 2.670,00 - R$ 57,45 (salário-base - valor IR) = R$ 2.612,55.
+Resultado: R$ 2.612,55.
+console.log("salario bruto ", salarioBruto)
+console.log("inss ", inss)
+console.log("salario base ", salarioBase)
+console.log("ir ", ir)
+console.log("salario liquido " + salarioLiquido)*/
+
+console.log("Recendo um sálario bruto de "+ salarioBruto.toFixed(2) + " se desconta R$ " + inss.toFixed(2) + " de INSS e R$ " + ir.toFixed(2) + " de IR. Restando um salário líquido de R$ " + salarioLiquido.toFixed(2) )
+
