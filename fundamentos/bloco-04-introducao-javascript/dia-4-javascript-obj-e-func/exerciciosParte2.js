aa5b43b4b4b95422956a726ffe512bf527963a98
dedojs <xandresousax@gmail.com>
2021-12-01 19:42:02 -0300

    exe functions

---
+++ fundamentos/bloco-04-introducao-javascript/dia-4-javascript-obj-e-func/exerciciosParte2.js
@@ -0,0 +1,64 @@
+//1
+/*
+function palindrome (palavra){
+    let pala = palavra
+    //console.log(pala)
+    let invertida =[]
+    invertida.push(pala.split('').reverse())
+    let invert = invertida[0]
+    //console.log(invert)
+    let iguais = 0;
+    for(let i = 0; i<=pala.length-1; i+=1){
+        
+            //console.log( pala[i] + ' = ' + invert[i])
+        if(pala[i] === invert[i]){
+            iguais +=1
+            //console.log(iguais)
+        }else{
+            return false
+        }
+}
+
+    if (iguais == palavra.length){
+        return true
+    }
+    
+}
+
+
+console.log(palindrome('ovo'))
+*/
+
+//2
+/*
+let teste = [2,3,6,7,10,1]
+function max_index(array){
+    let maior = Math.max.apply(null, array)
+    console.log(teste.indexOf(maior))
+    
+}
+
+max_index(teste)
+*/
+
+//3
+/*
+let teste = [2,3,6,7,10,0,-3]
+function min_index(array){
+    let menor = Math.min.apply(null, array)
+    console.log(teste.indexOf(menor))
+    
+}
+
+min_index(teste)
+*/
+
+4//
+let nomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']
+
+function max_char(array){
+    
+
+}
+
+
