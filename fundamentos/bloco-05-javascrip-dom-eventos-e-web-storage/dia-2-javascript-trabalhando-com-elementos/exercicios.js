45cce00ceb57127cd03d395bb6bed9decedc0ad2
dedojs <xandresousax@gmail.com>
2021-12-06 18:51:51 -0300

    exercicio 7

--- fundamentos/bloco-05-javascrip-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicios.js
+++ fundamentos/bloco-05-javascrip-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicios.js
@@ -19,5 +19,19 @@ let p = document.createElement('p')
 section.appendChild(p)
 p.innerText='Estudar na Trybe é muito bom'
 
+//5 Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
+let section2 = document.createElement('section')
+section2.className = 'left-content'
+main.appendChild(section2)
 
+//6 Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
+let section3 = document.createElement('section')
+section3.className = 'right-content'
+main.appendChild(section3)
+
+//7 Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do section criado no passo 5;
+let image = document.createElement('img')
+image.className ='small-image'
+image.src = 'https://picsum.photos/200'
+section2.appendChild(image)
 
