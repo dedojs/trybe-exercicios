91fba15e99e7fbd0ecfd8fe35f7ef560183cb080
dedojs <xandresousax@gmail.com>
2021-12-07 06:15:43 -0300

    arquivos do dia 5.2

--- fundamentos/bloco-05-javascrip-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicios.js
+++ fundamentos/bloco-05-javascrip-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicios.js
@@ -1,60 +1,71 @@
- //1 Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
-let titulo = document.createElement('h1')
-titulo.innerText = "Exercício 5.2 - JavaScript DOM"
-titulo.className ='title'
-document.body.appendChild(titulo)
+//1 Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
+let titulo = document.createElement("h1");
+titulo.innerText = "Exercício 5.2 - JavaScript DOM";
+titulo.className = "title";
+document.body.appendChild(titulo);
 //console.log(titulo)
 
 //2 - Adicione a tag main com a classe main-content como filho da tag body ;
-let main = document.createElement('main')
-main.className = 'main-content'
-document.body.appendChild(main)
+let main = document.createElement("main");
+main.className = "main-content";
+document.body.appendChild(main);
 
 //3 - Adicione a tag section com a classe center-content como filho da tag main criada no passo 2;
-let section = document.createElement('section')
-main.appendChild(section)
-section.className = 'center-content'
+let section = document.createElement("section");
+main.appendChild(section);
+section.className = "center-content";
 
 //4 Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
-let p = document.createElement('p')
-section.appendChild(p)
-p.className = 'description'
-p.innerText='Estudar na Trybe é muito bom'
+let p = document.createElement("p");
+section.appendChild(p);
+p.className = "description";
+p.innerText = "Estudar na Trybe é muito bom";
 
 //5 Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
-let section2 = document.createElement('section')
-section2.className = 'left-content'
-main.appendChild(section2)
+let section2 = document.createElement("section");
+section2.className = "left-content";
+main.appendChild(section2);
 
 //6 Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
-let section3 = document.createElement('section')
-section3.className = 'right-content'
-main.appendChild(section3)
+let section3 = document.createElement("section");
+section3.className = "right-content";
+main.appendChild(section3);
 
 //7 Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do section criado no passo 5;
-let image = document.createElement('img')
-image.className ='small-image'
-image.src = 'https://picsum.photos/200'
-section2.appendChild(image)
+let image = document.createElement("img");
+image.className = "small-image";
+image.src = "https://picsum.photos/200";
+section2.appendChild(image);
 
 //8 Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um , dois , três , ... como valores da lista. Essa lista deve ser filha do section criado no passo 6;
-let listaItens = ['Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez']
-let lista = document.createElement('ul')
-section3.appendChild(lista)
+let listaItens = [
+  "Um",
+  "Dois",
+  "Três",
+  "Quatro",
+  "Cinco",
+  "Seis",
+  "Sete",
+  "Oito",
+  "Nove",
+  "Dez",
+];
+let lista = document.createElement("ul");
+section3.appendChild(lista);
 
-for(let i = 0; i<listaItens.length; i+=1){
-    let numListItem = document.createElement('li')
-    numListItem.innerText = listaItens[i]
-    numListItem.className = 'list-item'
-    lista.appendChild(numListItem)
+for (let i = 0; i < listaItens.length; i += 1) {
+  let numListItem = document.createElement("li");
+  numListItem.innerText = listaItens[i];
+  numListItem.className = "list-item";
+  lista.appendChild(numListItem);
 }
 
 //9 Adicione 3 tags h3 , todas sendo filhas do main criado no passo 2.
-for(let i = 0; i<3; i+=1){
-    let h3 = document.createElement('h3')
-    h3.className = 'description'
-    
-    main.appendChild(h3)
+for (let i = 0; i < 3; i += 1) {
+  let h3 = document.createElement("h3");
+  h3.className = "description";
+
+  main.appendChild(h3);
 }
 
 //10 Adicione a classe title na tag h1 criada;
@@ -64,21 +75,17 @@ for(let i = 0; i<3; i+=1){
 //h3.className = 'description'
 
 //12 Remova a section criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild() ;
-main.removeChild(section2)
+main.removeChild(section2);
 
 //13 Centralize a section criado no passo 6 (aquele que possui a classe right-content ). Dica: para centralizar, basta configurar o margin-right: auto da section ;
-section3.style.marginRight = 'auto'
+section3.style.marginRight = "auto";
 
 //14 Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
-main.style.backgroundColor = 'green'
+main.style.backgroundColor = "green";
 
 //15 Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
-let item = document.getElementsByClassName ('list-item')
-lista.removeChild(item[9])
+let item = document.getElementsByClassName("list-item");
+lista.removeChild(item[9]);
 
-let item2 = lista.lastElementChild
-lista.removeChild(item2)
-
-
-
-
+let item2 = lista.lastElementChild;
+lista.removeChild(item2);
--- fundamentos/bloco-05-javascrip-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-05-javascrip-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -22,84 +22,81 @@ console.log(document.getElementById('start').nextElementSibling) // <p>elemento<
 // parte 2
 
 //1
-let pai = document.getElementById('pai')
-console.log(pai)
+let pai = document.getElementById("pai");
+console.log(pai);
 
-let irmao = document.createElement('p')
-let text = document.createTextNode("filho do pai")
-irmao.appendChild(text)
+let irmao = document.createElement("p");
+let text = document.createTextNode("filho do pai");
+irmao.appendChild(text);
 
-let sobrinho = document.createElement('p')
-sobrinho.innerText ='Sobrinho di de Lia'
+let sobrinho = document.createElement("p");
+sobrinho.innerText = "Sobrinho di de Lia";
 
 pai.appendChild(sobrinho);
 pai.appendChild(irmao);
 
 //2
-let segundoFilho = document.getElementById('elementoOndeVoceEsta')
+let segundoFilho = document.getElementById("elementoOndeVoceEsta");
 
-let terceiroFilhoDoFilho = document.createElement('p')
-let textfilho = document.createTextNode('terceiroFilhoDoElemento')
-terceiroFilhoDoFilho.appendChild(textfilho)
-segundoFilho.appendChild(terceiroFilhoDoFilho)
+let terceiroFilhoDoFilho = document.createElement("p");
+let textfilho = document.createTextNode("terceiroFilhoDoElemento");
+terceiroFilhoDoFilho.appendChild(textfilho);
+segundoFilho.appendChild(terceiroFilhoDoFilho);
 
 //3
-let primeiroFilhoDoFilho = document.getElementById('elementoOndeVoceEsta').firstElementChild
+let primeiroFilhoDoFilho = document.getElementById(
+  "elementoOndeVoceEsta"
+).firstElementChild;
 
-let fiDoPrimeiroFi = document.createElement('section')
-let textfi = document.createTextNode('Fi do Primeiro filho do filho')
-fiDoPrimeiroFi.appendChild(textfi)
+let fiDoPrimeiroFi = document.createElement("section");
+let textfi = document.createTextNode("Fi do Primeiro filho do filho");
+fiDoPrimeiroFi.appendChild(textfi);
 
-primeiroFilhoDoFilho.appendChild(fiDoPrimeiroFi)
+primeiroFilhoDoFilho.appendChild(fiDoPrimeiroFi);
 
 //4
-let terceiroFilhoDoElemento = fiDoPrimeiroFi.parentNode.nextSibling.nextSibling.nextSibling.nextSibling;
-
+let terceiroFilhoDoElemento =
+  fiDoPrimeiroFi.parentNode.nextSibling.nextSibling.nextSibling.nextSibling;
 
 // parte 3 removendo
 
-console.log(document.getElementById('paiDoPai').children)
+console.log(document.getElementById("paiDoPai").children);
 
-let primeiroFilho = document.getElementById('pai').firstElementChild
+let primeiroFilho = document.getElementById("pai").firstElementChild;
 
-pai.removeChild(primeiroFilho)
+pai.removeChild(primeiroFilho);
 
-let terceiroFilhoDoPai = document.getElementById('pai').firstElementChild.nextElementSibling
+let terceiroFilhoDoPai =
+  document.getElementById("pai").firstElementChild.nextElementSibling;
 //console.log('3 =', terceiroFilhoDoPai)
- pai.removeChild(terceiroFilhoDoPai)
+pai.removeChild(terceiroFilhoDoPai);
 
- let quartoFilhoDoPai = document.getElementById('pai').firstElementChild.nextElementSibling
- //console.log('4 =', quartoFilhoDoPai)
- pai.removeChild(quartoFilhoDoPai)
+let quartoFilhoDoPai =
+  document.getElementById("pai").firstElementChild.nextElementSibling;
+//console.log('4 =', quartoFilhoDoPai)
+pai.removeChild(quartoFilhoDoPai);
 
-let quintoFilho = document.getElementById('pai').firstElementChild.nextElementSibling.nextElementSibling
+let quintoFilho =
+  document.getElementById("pai").firstElementChild.nextElementSibling
+    .nextElementSibling;
 //console.log('5 =', quintoFilho)
-pai.removeChild(quintoFilho)
+pai.removeChild(quintoFilho);
 
-let sextoFilho = document.getElementById('pai').firstElementChild.nextElementSibling
+let sextoFilho =
+  document.getElementById("pai").firstElementChild.nextElementSibling;
 //console.log('6 =', sextoFilho)
-pai.removeChild(sextoFilho)
+pai.removeChild(sextoFilho);
 
-let fidofi = segundoFilho.firstElementChild.nextElementSibling
+let fidofi = segundoFilho.firstElementChild.nextElementSibling;
 //console.log('f',fidofi)
-segundoFilho.removeChild(fidofi)
+segundoFilho.removeChild(fidofi);
 
-let irmaosdosfi = segundoFilho.firstElementChild.nextElementSibling
+let irmaosdosfi = segundoFilho.firstElementChild.nextElementSibling;
 //console.log('f1',irmaosdosfi)
-segundoFilho.removeChild(irmaosdosfi)
+segundoFilho.removeChild(irmaosdosfi);
 
-let firstFi = segundoFilho.firstElementChild
-let fidoFirst = firstFi.firstElementChild
-firstFi.removeChild(fidoFirst)
-
-
-
-
+let firstFi = segundoFilho.firstElementChild;
+let fidoFirst = firstFi.firstElementChild;
+firstFi.removeChild(fidoFirst);
 
 //console.log(document.getElementById('pai').childNodes)
-
-
-
-
-
-
