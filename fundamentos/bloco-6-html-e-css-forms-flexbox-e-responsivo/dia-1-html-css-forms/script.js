38e074edff4d05f844d119efeaf1023013843fee
dedojs <xandresousax@gmail.com>
2022-01-04 21:49:03 -0300

    ex curriculo

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,18 +1,15 @@
-// SELETORES
-const INPUT_TEXT = document.querySelector("#input-text");
-const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
-const HREF_LINK = document.querySelector("#href");
+const states = document.getElementById('estados');
+const estadosBrasil = ["Acre","Alagoas","Amapá","Amazonas","Bahia","Ceará","Espírito Santo","Goiás","Maranhão","Mato Grosso","Mato Grosso do Sul","Minas Gerais","Pará","Paraíba","Paraná","Pernambuco","Piauí","Rio de Janeiro","Rio Grande do Norte","Rio Grande do Sul","Rondônia","Roraima","Santa Catarina","São Paulo","Sergipe","Tocantins", "Distrito Federal" ]
 
-HREF_LINK.addEventListener('click',(target) => {
-    target.preventDefault();
-} )
-
-INPUT_CHECKBOX.addEventListener('click', (target) => {
-    target.preventDefault();
-})
-
-INPUT_TEXT.addEventListener('keypress', (event) => {
-    if (event.key !== 'a') {
-    event.preventDefault();
+function criarEstados () {
+    for (let i = 0; i < estadosBrasil.length; i += 1) {
+        const uf = document.createElement('option')
+        uf.innerText = estadosBrasil[i]
+        uf.value = estadosBrasil[i]
+        states.appendChild(uf)
     }
-})
+    
+}
+criarEstados()
+
+
