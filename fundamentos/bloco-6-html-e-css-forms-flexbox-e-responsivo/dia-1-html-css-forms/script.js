91e5802009a015b3f4480c2a9589e6d8e657ffa5
dedojs <xandresousax@gmail.com>
2022-01-04 22:27:15 -0300

    ex curriculo

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,5 +1,7 @@
 const states = document.getElementById('estados');
 const estadosBrasil = ["Acre","Alagoas","Amapá","Amazonas","Bahia","Ceará","Espírito Santo","Goiás","Maranhão","Mato Grosso","Mato Grosso do Sul","Minas Gerais","Pará","Paraíba","Paraná","Pernambuco","Piauí","Rio de Janeiro","Rio Grande do Norte","Rio Grande do Sul","Rondônia","Roraima","Santa Catarina","São Paulo","Sergipe","Tocantins", "Distrito Federal" ]
+const enviar = document.getElementById('btnEnviar');
+const imprimir = document.getElementById('impressao');
 
 function criarEstados () {
     for (let i = 0; i < estadosBrasil.length; i += 1) {
@@ -12,4 +14,20 @@ function criarEstados () {
 }
 criarEstados()
 
+function enviarManual(event) {
+    event.preventDefault()
+    validarNome()
+}
 
+enviar.addEventListener('click', enviarManual);
+
+function validarNome() {
+    let name = document.querySelector('[name=nome]')
+    if (name.value.length > 40){
+        alert('Informação inválida')
+    } else if (name.value.length === 0){
+        alert('Preenchimento obrigatório')
+    }
+    imprimir.innerText = 'Nome = ' + name.value
+    /* console.log(name.value) */
+}
