4c862a98e6e5736318f7d050f03c8f8ecad7f4df
dedojs <xandresousax@gmail.com>
2022-01-20 18:10:12 -0300

    aula ao vivo

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/aulaaovivo.js
@@ -0,0 +1,20 @@
+const num = [2, 5, 6, 7, 8, 20]
+let maior = []
+/* for(let index = 0; index < num.length; index +=1) {
+    if (num[index] > 10) {
+        maior.push(num[index])
+    }
+} */
+
+console.log(maior)
+
+const nums = (arr) => {
+    for(let index = 0; index < num.length; index +=1) {
+        if (num[index] > 10) {
+            maior.push(num[index])
+        }
+    }
+}
+nums(num)
+console.log(maior)
+
