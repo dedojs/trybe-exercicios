3e90a03034613a725a42d892ec9f6c6aa3693a7e
dedojs <xandresousax@gmail.com>
2022-06-24 14:26:32 -0300

    atualizando

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/index.js
@@ -1,9 +1,12 @@
+const bodyParser = require('body-parser');
 const express = require('express')
 const app = express()
 const port = 3000
 
 const Author = require('./models/Author')
 const Book = require('./models/Book')
+
+app.use(bodyParser.json());
 
 app.get('/authors', async (req, res) => {
   const authors = await Author.getAll();
@@ -18,6 +21,17 @@ app.get('/authors/:id', async (req, res) => {
   res.status(200).json(author);
 })
 
+app.post('/authors', async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  if (Author.isValid(first_name, middle_name, last_name)) {
+    return res.status(400).json({ message: 'Data Invalid'})
+  };
+  await Author.createAuthor(first_name, middle_name, last_name);
+
+  res.status(201).json({ message: 'create success'})
+})
+
 app.get('/books', async (req, res) => {
   const books = await Book.getAllBooks();
   res.status(200).json(books);
@@ -26,7 +40,20 @@ app.get('/books', async (req, res) => {
 app.get('/books/:id', async (req, res) => {
   const { id } = req.params;
   const books = await Book.getByAuthorId(id);
+
+  if(!books) return res.status(404).json({message:'Not Found'})
+  
   res.status(200).json(books);
+})
+
+app.post('/books', async (req, res) => {
+  const { title, author_id } = req.body
+  if (!Book.isvalid(title, author_id)) {
+    return res.status(400).json({ message: 'Dados inválidos'})
+  }
+  await Book.createBook(title, author_id)
+
+  res.status(201).json({ message: 'Livro criado com sucesso!'})
 })
 
 
--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/models/Book.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/models/Book.js
@@ -13,11 +13,28 @@ const getByAuthorId = async (id) => {
     `SELECT title from model_example.books
     where author_id = ?;`, [id]
   )
-  
+
+  if(author_id.length === 0) return null
+
   return author_id
+}
+
+const isvalid = (title, author_id) => {
+  if (!title || title.length < 3) return false
+  if (!author_id || author_id.length === 0) return false
+return true
+}
+
+const createBook = async (title, author_id) => {
+  connection.execute (
+    'INSERT INTO model_example.books (title, author_id) VALUES (?, ?);',
+    [title, author_id]
+  )
 }
 
 module.exports = {
   getAllBooks,
   getByAuthorId,
+  isvalid,
+  createBook,
 };
