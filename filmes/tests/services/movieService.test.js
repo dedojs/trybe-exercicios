8d2ec0089b6622a4d7c8ced519a3f92550f6f64f
dedojs <xandresousax@gmail.com>
2022-07-07 15:29:18 -0300

    atualizando

---
+++ filmes/tests/services/movieService.test.js
@@ -0,0 +1,55 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const MoviesModel = require('../../models/movieModel');
+const MoviesService =require('../../services/movieService');
+
+describe('Insere um novo filme no BD', () => {
+  const payloadMovie = {};
+  describe('quando o payload informado não é válido', () => {
+
+    it('retorna um boolean', async () => {
+      const response = await MoviesService.create(payloadMovie);
+
+      expect(response).to.be.a('boolean');
+    });
+
+    it('o boolean contém "false"', async () => {
+      const response = await MoviesService.create(payloadMovie);
+
+      expect(response).to.be.equal(false);
+    });
+
+  });
+
+  describe('quando é inserido com sucesso', () => {
+    const payloadMovie = {
+      title: 'Example Movie',
+      directedBy: 'Jane Dow',
+      releaseYear: 1999,
+    };
+
+    before(() => {
+      const ID_EXAMPLE = 1;
+      console.log('teste aqui')
+      sinon.stub(MoviesModel, 'create').resolves({ id: ID_EXAMPLE });
+    });
+
+    after(() => {
+      MoviesModel.create.restore();
+    })
+
+    it('retorna um objeto', async () => {
+      const response = await MoviesService.create(payloadMovie);
+
+      expect(response).to.be.a('object');
+    });
+
+    it('tal objeto possui o "id" de novo filme inserido', async () => {
+      const response = await MoviesService.create(payloadMovie);
+
+      expect(response).to.have.a.property('id')
+    });
+
+  });
+});
