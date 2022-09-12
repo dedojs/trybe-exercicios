4d5d5630729fd5b18c59d44447513405f0900751
dedojs <xandresousax@gmail.com>
2022-09-12 19:47:26 -0300

    day 1 mongoose crud and tests model

---
+++ back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-1-mongoose-e-arquitetura-msccamada-model/connection.ts
@@ -0,0 +1,9 @@
+import { connect } from 'mongoose';
+
+const options = {
+  user: 'user',
+  pass: 'password',
+  autoIndex: false,
+  dbName: 'glassesStore',
+}
+connect('mongodb://localhost:27017/', options);
